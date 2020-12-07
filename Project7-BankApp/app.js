295232952cda6290d5ef09b505df8f87be076393
Enrique Ferrer <emferrer4@gmail.com>
2020-12-07 17:24:23 +0800

    updated design

--- Project7-BankApp/app.js
+++ Project7-BankApp/app.js
@@ -11,6 +11,9 @@ let User = function(){
 
 let user = new User;
 
+
+
+
 class Bank {
     constructor() {
         this.usersArray = [];
@@ -19,9 +22,13 @@ class Bank {
         let found = bank.usersArray.some(elem => elem.username === username)
         
         if (found) {
-            alert ("User already exists")
+            document.querySelector(".alert").classList.remove('hidden');
+            document.querySelector("#error").innerText = "User already exist";
+            setTimeout(function () { document.querySelector(".alert").classList.add('hidden'); }, 3000);
         } else if (password !== confirmPassword){
-            alert ("Passwords must match")
+            document.querySelector(".alert").classList.remove('hidden');
+            document.querySelector("#error").innerText = "Passwords must match";
+            setTimeout(function () { document.querySelector(".alert").classList.add('hidden'); }, 3000);
         } else { 
             let newUser = new User(username,password);
             newUser.login = true;
@@ -44,9 +51,13 @@ class Bank {
         let userLogIn = bank.usersArray.find(elem => elem.username === user);
 
         if (!found){
-            alert ("User does not exist")
+            document.querySelector(".alert").classList.remove('hidden');
+            document.querySelector("#error").innerText = "User does not exist";
+            setTimeout(function () { document.querySelector(".alert").classList.add('hidden'); }, 3000);
         } else if (userLogIn.password !== password) {
-            alert("Wrong password!")
+            document.querySelector(".alert").classList.remove('hidden');
+            document.querySelector("#error").innerText = "Wrong password";
+            setTimeout(function () { document.querySelector(".alert").classList.add('hidden'); }, 3000);
         } else {
             userLogIn.login = true;
             resetForms();
@@ -77,28 +88,48 @@ class Bank {
         document.querySelector(".container").classList.remove('hidden');
     }
     deposit(user, amount) {
-        let foundUser = this.usersArray.find(elem => elem.login === true);
-        foundUser.balance += amount;
-        resetForms();
-        this.updateProfile();
-        startStateTwo();
-
-        console.log("Deposited " + amount + " to " + user.username);
-        console.table(bank.usersArray);
+        const conf = confirm("Are you sure you want to deposit " + formatter.format(amount) + "?");
+        if (conf == true) {
+            let foundUser = this.usersArray.find(elem => elem.login === true);
+            foundUser.balance += amount;
+            resetForms();
+            this.updateProfile();
+            startStateTwo();
+    
+            document.querySelector(".success").classList.remove('hidden');
+            document.querySelector("#success-msg").innerText = "Deposited " + formatter.format(amount) + " to " + user.username;
+            setTimeout(function () { document.querySelector(".success").classList.add('hidden'); }, 3000);
+    
+            console.log("Deposited " + formatter.format(amount) + " to " + user.username);
+            console.table(bank.usersArray);
+        } else {
+            return
+        }
     }
     withdraw(user, amount) {
         let foundUser = this.usersArray.find(elem => elem.login === true);
 
         if (foundUser.balance - amount < 0) {
-            alert ("Not enough money!")
+            document.querySelector(".alert").classList.remove('hidden');
+            document.querySelector("#error").innerText = "Insufficient funds";
+            setTimeout(function () { document.querySelector(".alert").classList.add('hidden'); }, 3000);
         } else {
-            foundUser.balance -= amount;
-            resetForms();
-            this.updateProfile();
-            startStateTwo();
-
-            console.log("Withdrew " + amount + " from " + user.username);
-            console.table(bank.usersArray);
+            const conf = confirm("Are you sure you want to withdraw " + formatter.format(amount) + "?");
+            if (conf == true) {
+                foundUser.balance -= amount;
+                resetForms();
+                this.updateProfile();
+                startStateTwo();
+    
+                document.querySelector(".success").classList.remove('hidden');
+                document.querySelector("#success-msg").innerText = "Deposited " + formatter.format(amount) + " to " + user.username;
+                setTimeout(function () { document.querySelector(".success").classList.add('hidden'); }, 3000);
+    
+                console.log("Withdrew " + formatter.format(amount) + " from " + user.username);
+                console.table(bank.usersArray);
+            } else {
+                return
+            }
         }
     }
     send(user, amount, receiver) {
@@ -107,26 +138,46 @@ class Bank {
         let receiverUser = this.usersArray.find(elem => elem.username === receiver);
 
         if (!found){
-            alert ("User " + receiver + " does not exist")
+            document.querySelector(".alert").classList.remove('hidden');
+            document.querySelector("#error").innerText = "User " + receiver + " does not exist";
+            setTimeout(function () { document.querySelector(".alert").classList.add('hidden'); }, 3000);
         } else if (sender.balance - amount < 0) {
-            alert ("Not enough money!")
+            document.querySelector(".alert").classList.remove('hidden');
+            document.querySelector("#error").innerText = "Insufficient funds";
+            setTimeout(function () { document.querySelector(".alert").classList.add('hidden'); }, 3000);
         } else if (sender.username === receiverUser.username) {
-            alert ("Cannot send money to the same account")
+            document.querySelector(".alert").classList.remove('hidden');
+            document.querySelector("#error").innerText = "Cannot send money to the same account";
+            setTimeout(function () { document.querySelector(".alert").classList.add('hidden'); }, 3000);
         }else {
-            sender.balance -= amount;
-            receiverUser.balance += amount;
-            resetForms();
-            this.updateProfile();
-            startStateTwo();
-
-            console.log("Sent " + amount + " to " + receiverUser.username) 
-            console.table(bank.usersArray)
+            const conf = confirm("Are you sure you want to send " + formatter.format(amount) + " to " + receiverUser.username + "?");
+            if (conf == true) {
+                sender.balance -= amount;
+                receiverUser.balance += amount;
+                resetForms();
+                this.updateProfile();
+                startStateTwo();
+    
+                document.querySelector(".success").classList.remove('hidden');
+                document.querySelector("#success-msg").innerText = "Sent " + formatter.format(amount) + " to " + receiverUser.username;
+                setTimeout(function () { document.querySelector(".success").classList.add('hidden'); }, 3000);
+    
+                console.log("Sent " + amount + " to " + receiverUser.username) 
+                console.table(bank.usersArray)
+            } else {
+                return
+            }
         }
     }
     changeName(newName) {
         let foundUser = this.usersArray.find(elem => elem.login === true);
         foundUser.name = newName;
         document.querySelector("#get-name").innerText = foundUser.name;
+        
+        document.querySelector(".success").classList.remove('hidden');
+        document.querySelector("#success-msg").innerText = "Successfully changed name";
+        setTimeout(function () { document.querySelector(".success").classList.add('hidden'); }, 3000);
+
         console.log("Changed name to " + newName);
     }
     changeUsername(newUsername){
@@ -134,30 +185,45 @@ class Bank {
         let foundUser = this.usersArray.find(elem => elem.login === true);
 
         if (found) {
-            alert ("User already exists!")
+            document.querySelector(".alert").classList.remove('hidden');
+            document.querySelector("#error").innerText = "User already exists!";
+            setTimeout(function () { document.querySelector(".alert").classList.add('hidden'); }, 3000);
         } else {
             foundUser.username = newUsername;
+
             document.querySelector("#get-user").innerText = foundUser.username;
             console.log("Changed username to " + newUsername);
+
+            document.querySelector(".success").classList.remove('hidden');
+            document.querySelector("#success-msg").innerText = "Successfully changed username";
+            setTimeout(function () { document.querySelector(".success").classList.add('hidden'); }, 3000);
         }
     }
     changePic(newPic){
         let foundUser = this.usersArray.find(elem => elem.login === true);
         foundUser.picture = newPic;
         resetForms();
+
+        document.querySelector(".success").classList.remove('hidden');
+        document.querySelector("#success-msg").innerText = "Successfully changed picture";
+        setTimeout(function () { document.querySelector(".success").classList.add('hidden'); }, 3000);
     }
     updateProfile(){
         let foundUser = bank.usersArray.find(elem => elem.login === true);
         
         document.querySelector("#get-name").innerText = foundUser.name;
+        document.querySelector("#logged-user").innerText = foundUser.username;
         document.querySelector("#get-user").innerText = foundUser.username;
         document.querySelector("#get-balance").innerText = formatter.format(foundUser.balance);
+        document.querySelector("#get-balance-2").innerText = formatter.format(foundUser.balance);
         document.querySelector("#avatar").src = foundUser.picture;
     }
 }
 
 const bank = new Bank();
 
+
+/************* EVENT LISTENERS FOR FORMS/BUTTONS *************/
 // REGISTRATION FORM // 
 let regForm = document.querySelector("#register-form");
 function handleReg(e){
@@ -204,7 +270,7 @@ function handleWithdraw(e){
 }
 withdrawForm.addEventListener('submit', handleWithdraw);
 
-// SEND TO FORM // 
+// SEND-TO FORM // 
 let sendForm = document.querySelector("#send-form");
 function handleSend(e){
     e.preventDefault();
@@ -215,6 +281,7 @@ function handleSend(e){
 };
 sendForm.addEventListener('submit', handleSend);
 
+let pictureForm = document.querySelector("#picture-form");
 // Reset all forms 
 function resetForms() {
     regForm.reset();
@@ -237,7 +304,7 @@ document.querySelector("#change-name-btn").addEventListener('click', function(){
 
 // Change Username
 document.querySelector("#change-username-btn").addEventListener('click', function(){
-    let newUsername = prompt ("Enter new name"); 
+    let newUsername = prompt("Enter new name").toLowerCase(); 
     if(!newUsername) {
         return
     } else {
@@ -256,39 +323,38 @@ window.addEventListener('load', function() {
     });
 });
 
+// Close Alert Button - error
+document.querySelector(".close-btn").addEventListener('click', function(){
+    document.querySelector(".alert").classList.add("hidden");
+});
+
+// Close Alert Button - success
+document.querySelector(".close-btn").addEventListener('click', function(){
+    document.querySelector(".success").classList.add("hidden");
+});
 
 /************* DOM MANIPULATION, SHOWING DIFFERENT ACTIVE STATES*************/
 /* For first state -- NO user LOGGED IN */
 function startStateOne(){
-    document.querySelector(".about").classList.remove("hidden");
-    document.querySelector(".register").classList.add("hidden");
-    document.querySelector(".login").classList.add("hidden");
-    document.querySelector("#about-tab").classList.add("active");
-    document.querySelector("#register-tab").classList.remove("active");
-    document.querySelector("#login-tab").classList.remove("active");
+    document.querySelector('#container-one').classList.remove("panel-active")
     resetForms();   
 }
-    // About
-document.querySelector("#about-tab").addEventListener('click', function(){
-    startStateOne();
+
+const signUpBtn = document.querySelector('#register-tab');
+const signInBtn = document.querySelector('#login-tab');
+const container = document.querySelector('#container-one');
+
+signUpBtn.addEventListener('click', () => {
+	container.classList.add("panel-active");
 });
-    // Register
-document.querySelector("#register-tab").addEventListener('click', function(){
-    document.querySelector(".about").classList.add("hidden");
-    document.querySelector(".register").classList.remove("hidden");
-    document.querySelector(".login").classList.add("hidden");
-    document.querySelector("#about-tab").classList.remove("active")
-    document.querySelector("#register-tab").classList.add("active")
-    document.querySelector("#login-tab").classList.remove("active")
+
+signInBtn.addEventListener('click', () => {
+	container.classList.remove("panel-active");
 });
-    // Log-In
-document.querySelector("#login-tab").addEventListener('click', function(){
-    document.querySelector(".about").classList.add("hidden");
-    document.querySelector(".register").classList.add("hidden");
-    document.querySelector(".login").classList.remove("hidden");
-    document.querySelector("#about-tab").classList.remove("active");
-    document.querySelector("#register-tab").classList.remove("active");
-    document.querySelector("#login-tab").classList.add("active");
+
+let forgotPassword = document.querySelector("#forgot-password");
+forgotPassword.addEventListener('click', () => {
+    prompt ("Enter your username");
 });
 
 /* For second state -- User LOGGED IN */
@@ -304,6 +370,7 @@ function startStateTwo(){
     document.querySelector("#withdraw-tab").classList.remove("active");
     document.querySelector("#send-tab").classList.remove("active");
     document.querySelector("#profile-tab").classList.remove("active");
+    document.querySelector(".transaction").classList.add("hidden");
     resetForms();
 }
     // Home 
@@ -323,6 +390,7 @@ document.querySelector("#deposit-tab").addEventListener('click', function(){
     document.querySelector("#withdraw-tab").classList.remove("active");
     document.querySelector("#send-tab").classList.remove("active");
     document.querySelector("#profile-tab").classList.remove("active");
+    document.querySelector(".transaction").classList.remove("hidden");
 });
     // Withdraw
 document.querySelector("#withdraw-tab").addEventListener('click', function(){
@@ -337,6 +405,8 @@ document.querySelector("#withdraw-tab").addEventListener('click', function(){
     document.querySelector("#withdraw-tab").classList.add("active");
     document.querySelector("#send-tab").classList.remove("active");
     document.querySelector("#profile-tab").classList.remove("active");
+    document.querySelector(".transaction").classList.remove("hidden");
+
 });
     // Send Money
 document.querySelector("#send-tab").addEventListener('click', function(){
@@ -351,6 +421,8 @@ document.querySelector("#send-tab").addEventListener('click', function(){
     document.querySelector("#withdraw-tab").classList.remove("active");
     document.querySelector("#send-tab").classList.add("active");
     document.querySelector("#profile-tab").classList.remove("active");
+    document.querySelector(".transaction").classList.remove("hidden");
+
 });
 
     // Profile Tab
@@ -366,16 +438,22 @@ document.querySelector("#profile-tab").addEventListener('click', function(){
     document.querySelector("#withdraw-tab").classList.remove("active");
     document.querySelector("#send-tab").classList.remove("active");
     document.querySelector("#profile-tab").classList.add("active");
+    document.querySelector(".transaction").classList.add("hidden");
 
     bank.updateProfile();
 });
 
 
 
-// Helper Functions
+// Helper Function for number format
 const formatter = new Intl.NumberFormat('fil-PH', {
     style: 'currency',
     currency: 'PHP',
 });
+
+
   
 // console.log(formatter.format(2500)); /* ₱2,500.00 */
+
+
+

42c0ea4a232cea745100383b606ad65eb27f2fdb
Enrique Ferrer <emferrer4@gmail.com>
2020-12-16 23:15:42 +0800

    completed Level 2

--- Mini-Projects/MP-1/app.js
+++ Mini-Projects/MP-1/app.js
@@ -1,4 +1,23 @@
+let person = {
+    name: "",
+}
+
 document.querySelector(".hello-btn").addEventListener('click', () => {
-    console.log("Clicked!")
-    document.querySelector('.display-text').innerText = "Hello World !";
+    // console.log("Clicked!")
+    if (person.name !== ""){
+        document.querySelector('.display-text').innerText = `Hello, ${person.name}!`;
+    } else {
+        document.querySelector('.display-text').innerText = "Hello World !";
+    }
 });
+
+
+document.querySelector(".submit-btn").addEventListener('click', event =>{
+    person.name = document.querySelector('#name').value
+    console.log(person.name);
+    event.preventDefault();
+    // console.log("Clicked!!");
+    document.querySelector('.display-text').innerText = `${person.name}`;
+    document.querySelector(".name-form").reset();
+})
+

d36c49f3e6742f65903fd1176265cec896ec2e41
Enrique Ferrer <emferrer4@gmail.com>
2020-12-17 10:51:44 +0800

    completed Level 3

--- Mini-Projects/MP-1/app.js
+++ Mini-Projects/MP-1/app.js
@@ -4,6 +4,7 @@ let person = {
 
 document.querySelector(".hello-btn").addEventListener('click', () => {
     // console.log("Clicked!")
+    document.querySelector(".say-goodbye-btn").innerHTML = "sayGoodbye";
     if (person.name !== ""){
         document.querySelector('.display-text').innerText = `Hello, ${person.name}!`;
     } else {
@@ -12,12 +13,58 @@ document.querySelector(".hello-btn").addEventListener('click', () => {
 });
 
 
-document.querySelector(".submit-btn").addEventListener('click', event =>{
+document.querySelector(".name-form").addEventListener('submit', event => {
+    event.preventDefault();
     person.name = document.querySelector('#name').value
     console.log(person.name);
-    event.preventDefault();
     // console.log("Clicked!!");
     document.querySelector('.display-text').innerText = `${person.name}`;
     document.querySelector(".name-form").reset();
-})
+    document.querySelector(".say-goodbye-btn").innerHTML = "sayGoodbye";
+});
 
+document.querySelector("#jerick").addEventListener('click', () =>{
+    console.log("Jerick");
+    person.name = "Jerick";
+    document.querySelector('.display-text').innerText = `${person.name}`;
+    document.querySelector(".say-goodbye-btn").innerHTML = "sayGoodbye";
+});
+
+document.querySelector("#pau").addEventListener('click', () =>{
+    console.log("Pau");
+    person.name = "Pau";
+    document.querySelector('.display-text').innerText = `${person.name}`;
+    document.querySelector(".say-goodbye-btn").innerHTML = "sayGoodbye";
+});
+
+document.querySelector("#john").addEventListener('click', () =>{
+    console.log("John");
+    person.name = "John";
+    document.querySelector('.display-text').innerText = `${person.name}`;
+    document.querySelector(".say-goodbye-btn").innerHTML = "sayGoodbye";
+});
+
+document.querySelector(".say-hello-btn").addEventListener('click', () => {
+    // console.log("Clicked!")
+    document.querySelector(".say-goodbye-btn").innerHTML = "sayGoodbye";
+    if (person.name !== ""){
+        document.querySelector('.display-text').innerText = `Hello, ${person.name}!`;
+    } else {
+        document.querySelector('.display-text').innerText = "Hello World !";
+    }
+});
+
+document.querySelector(".say-goodbye-btn").addEventListener('click', () => {
+    // console.log("Clicked!")
+    let btn = document.querySelector(".say-goodbye-btn");
+    
+    if (person.name !== "" && btn.innerHTML !== "Clear"){
+        document.querySelector('.display-text').innerText = `Goodbye, ${person.name}!`;
+        btn.innerHTML = "Clear";
+
+    } else if (btn.innerHTML === "Clear") {
+        person.name = "";
+        document.querySelector('.display-text').innerHTML = "&nbsp;";
+        btn.innerHTML = "sayGoodbye";
+    }
+});

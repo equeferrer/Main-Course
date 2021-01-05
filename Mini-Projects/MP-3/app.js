fab2affebd9cde48707ab63594c0689018cfe080
Enrique Ferrer <emferrer4@gmail.com>
2021-01-05 10:16:20 +0800

    changed styles, updated to form

--- Mini-Projects/MP-3/app.js
+++ Mini-Projects/MP-3/app.js
@@ -3,39 +3,45 @@ let currentDate = new Date();
 // document.querySelector("#date").innerText = "01/04/2021"
 document.querySelector("#date").innerText = currentDate.toLocaleDateString('en-US', options)
 
+const todoBox = document.querySelector(".add__container");
 
 const todoInput = document.querySelector(".add__description");
 const todoButton = document.querySelector(".add-btn");
 const todoList = document.querySelector("#task-container");
 
-todoButton.addEventListener('click', addToDo)
+todoBox.addEventListener('submit', handleSubmit);
+
+function handleSubmit(e){
+    e.preventDefault();
+	addToDo(e);
+}
 
 function addToDo(event) {
 	event.preventDefault();
 	if (todoInput.value !== "") {
 		// TO DO DIV
-	const todoDiv = document.createElement('div');
-	todoDiv.classList.add('item');
-// Checkmark button
-    const completedButton = document.createElement('button');
-    completedButton.innerHTML = '<i class="ion-ios-checkmark-outline"></i>'
-    completedButton.classList.add("item__complete");
-    todoDiv.appendChild(completedButton);
-// create task
-	const newToDo = document.createElement('div');
-	newToDo.innerText = todoInput.value;
-	newToDo.classList.add('item__description')
-	todoDiv.appendChild(newToDo)
+			const todoDiv = document.createElement('div');
+			todoDiv.classList.add('item');
+		// Checkmark button
+			const completedButton = document.createElement('button');
+			completedButton.innerHTML = '<i class="ion-ios-checkmark-outline"></i>'
+			completedButton.classList.add("item__complete");
+			todoDiv.appendChild(completedButton);
+		// create task
+			const newToDo = document.createElement('div');
+			newToDo.innerText = todoInput.value;
+			newToDo.classList.add('item__description')
+			todoDiv.appendChild(newToDo)
 
-//TRASH BUTTON
-	const trashButton = document.createElement('button');
-	trashButton.innerHTML = '<i class="ion-ios-close-outline">'
-	trashButton.classList.add("item__delete");
-	todoDiv.appendChild(trashButton);
-// Add to toDoDiv
-    todoList.appendChild(todoDiv)
-//Clear ToDo Input Value
-    todoInput.value="";
+		//TRASH BUTTON
+			const trashButton = document.createElement('button');
+			trashButton.innerHTML = '<i class="ion-ios-close-outline">'
+			trashButton.classList.add("item__delete");
+			todoDiv.appendChild(trashButton);
+		// Add to toDoDiv
+			todoList.appendChild(todoDiv)
+		//Clear ToDo Input Value
+			todoInput.value="";
 	} else if (todoInput.value === ""){
 		todoInput.classList.add('red-focus');
 		alert("Cannot add a blank task");

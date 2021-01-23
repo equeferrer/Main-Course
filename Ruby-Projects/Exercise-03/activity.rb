094b8649c1b51f9b63a0919f30dcda05c1a06a41
equeferrer <emferrer4@gmail.com>
2021-01-23 11:42:00 +0800

    update indent

--- Ruby-Projects/Exercise-03/activity.rb
+++ Ruby-Projects/Exercise-03/activity.rb
@@ -1,4 +1,4 @@
-# Activity 1: 
+# # Activity 1: 
 # We have a Confection class representing baked desserts. Your job is to create a new subclass of Confection, named Cupcake and Banana Cake. 
 # Cupcakes and Banana Cakes need to be baked just like other confections, but cupcakes also need to be frosted afterward. 
 # Write a prepare method for your Cupcake and Banana Cake class that prints “Baking at 350 degrees for 25 minutes.“, 
